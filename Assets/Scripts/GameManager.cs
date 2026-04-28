842463de58bae659e939f514c3ae758f57624141
DassuiGyo <dgorman@mail.bradley.edu>
2026-04-28 16:54:53 -0500

    Work Day | Level Design

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,15 +18,19 @@
         [SerializeField] private TextMeshProUGUI enemyRemain;
         [SerializeField] private GameObject platform1;
         [SerializeField] private TextMeshProUGUI doorOpen;
-
+        
         private int collectables;
         private DoorManager doorManager;
+
+        private bool levelOne = false;
+        private bool levelTwo;
+        private bool levelThree;
 
         private InputAction restart;
         private InputAction exit;
 
         /// <summary>
-        /// At the start of the scene all fields are set and enable
+        /// At the start of the scene all fields are set and enable | Sets the bools for each level when on that level
         /// </summary>
         private void Start()
         {
@@ -40,12 +44,29 @@
 
             collectables = 0;
             platform1.gameObject.SetActive(false);
+
+            #region Current Level Quest Functions
+
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                levelOne = true;
+                enemyRemain.text = "Find & Shoot red boxes\r\n- 2 remaining";
+            }
+
+            if (SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                levelTwo = true;
+                enemyRemain.text = "Find & Shoot red boxes\r\n- 3 remaining";
+            }
+            levelThree = false;
+
+            #endregion
         }
 
-        /// <summary>
-        /// When the script is disable/inactive so is the restart action
-        /// </summary>
-        private void OnDisable()
+    /// <summary>
+    /// When the script is disable/inactive so is the restart action
+    /// </summary>
+    private void OnDisable()
         {
             if (restart != null) { restart.performed -= RestartPerformed; }
         }
@@ -70,12 +91,13 @@
         }
 
         /// <summary>
-        /// Loads the scene listed as number 1 in the build settings
+        /// Loads the scene listed as number 1 in the build settings | Sets the quest and enemies for that level
         /// </summary>
-        public void LevelOne()
+        public void LevelOneSelect()
         {
             SceneManager.LoadScene(1);
         }
+
 
         #region Enemy Functions
 
@@ -84,31 +106,26 @@
         /// </summary>
         public void EnemyDestroyed()
         {
-            //Level 1 
 
             collectables++;
-        
-            if(collectables == 1)
+            Debug.Log("Enemy Destroyed");
+
+            //Level 1 
+            if (levelOne == true)
             {
-                enemyRemain.text = "Find & Shoot red boxes\r\n- 2 remaining";
+                if (collectables == 1) { enemyRemain.text = "Find & Shoot red boxes\r\n- 1 remaining"; }
+
+                if (collectables == 2) 
+                { 
+                    enemyRemain.text = "Find & Shoot red boxes\r\n- 0 remaining";
+                    enemyRemain.color = Color.green;
+
+                    doorManager.allEniemesGone();
+                    doorOpen.text = "Collect the VR Headset | Unlocked\r\n<s>- Completed other objectives to unlock";
+                    doorOpen.color = Color.green;
+                }
             }
 
-            if(collectables == 2)
-            {
-                enemyRemain.text = "Find & Shoot red boxes\r\n- 1 remaining";
-            }
-
-            if(collectables == 3)
-            {
-                enemyRemain.text = "<s>Find & Shoot red boxes\r\n- 0 remaining";
-                enemyRemain.color = Color.green;
-
-                doorManager.allEniemesGone();
-
-                doorOpen.text = "Collect the VR Headset | Unlocked\r\n<s>- Completed other objectives to unlock";
-                doorOpen.color = Color.green;
-            
-            }
         }
 
         public void PlatformSpawn()
@@ -116,10 +133,5 @@
             platform1.gameObject.SetActive(true);
         }
 
-        public void LevelOneEnemies()
-        {
-
-        }
-
-    #endregion
-}
+        #endregion
+    }
